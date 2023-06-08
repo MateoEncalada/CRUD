9d92c96363faffe5f77f34d5b6b1282ef6f06c52
Mateo Encalada <mateo.encalada@udla.edu.ec>
2023-06-08 09:07:14 -0500

    Agregar archivos de proyecto.

---
+++ Pages/ConexionBasecs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Pages
+{
+    public class ConexionBasecs
+    {
+
+        private static ConexionBasecs instance;
+        private readonly SqlConnection connection;
+
+        private ConexionBasecs()
+        {
+            string connectionString = "Data Source=(local); Initial Catalog=Facturacion; trusted_connection=yes; TrustServerCertificate=True;";
+            connection = new SqlConnection(connectionString);
+        }
+
+        public static ConexionBasecs Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ConexionBasecs();
+                }
+                return instance;
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
